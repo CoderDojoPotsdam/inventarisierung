b20042c06d6d85a898f212d572228862276aeff6
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-12-06 09:44:18 -0800

    first working scanner

---
+++ scanner/scan.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+cd "`dirname \"$0\"`"
+
+scanner_id_file="../scanner-id"
+
+if ! [ -f "$scanner_id_file" ]
+then
+  python -c"import random; print(random.randint(0, 1000000))" > "$scanner_id_file"
+fi
+scanner_id="`cat \"$scanner_id_file\"`"
+
+echo -n "Identifiziere den Computer: "
+read id
+
+dir=../hardware/"$id"/scan
+mkdir -p "$dir"
+cd "$dir"
+
+echo "Lese RAM"
+
+free -m > free-m.out
+sudo dmidecode -t memory > dmidecode-memory.out
+
+echo "Lese USB"
+
+lsusb > lsusb.out
+
+echo "Lese Netzwerk"
+
+ifconfig > ifconfig.out
+
+echo "Lese Festplatten"
+sudo fdisk -l > fdisk-l.out
+
+echo "Lese CPU"
+cat /proc/cpuinfo > proc_cpuinfo.out
+
+echo "Lese System"
+cat /etc/issue > etc_issue.out
+uname -a > uname-a.out
+lsb_release -a > lsb_release-a.out
+
+echo "Lese Hardwareinformationen"
+hwinfo > hwinfo.out
+
+echo "Beschreibe Besonderheiten"
+beschreibung=beschreibung.out
+echo "Besonderheiten von $id:" >> "$beschreibung"
+nano "$beschreibung"
+
+echo "Speichere Kopie"
+copy="scanner_$scanner_id"
+mkdir -p "$copy"
+cp *.out "$copy"
+
+echo "Speichere ins git."
+
+git pull
+git add .
+git commit -m"$id inventarisiert."
+git push
+
+
+echo "Fertig, ausschalten."
