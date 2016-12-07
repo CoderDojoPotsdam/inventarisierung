13f165148ebfbeca8cd921bf32db518bbf15b85d
Nicco Kunzmann - inventarisierung <niccokunzmann@rambler.ru>
2016-12-07 14:17:18 +0100

    added scan command installation

--- scanner/install.sh
+++ scanner/install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 sudo apt-get -y install dmidecode hwinfo python3 smartmontools
+
+if ! which scan
+then
+  (
+    echo
+    echo "function scan() {"
+    echo "  `dirname \"$0\"`/scan.sh"
+    echo "}"
+    echo
+  ) >> ~/.bashrc
+fi
