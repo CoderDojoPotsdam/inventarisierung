9acb64b39589b89c976207e95d601b675ddfb83b
Nicco Kunzmann - inventarisierung <niccokunzmann@rambler.ru>
2016-12-09 09:42:59 +0100

    added othe rpackags

--- scanner/install.sh
+++ scanner/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo apt-get -y install dmidecode hwinfo python3 smartmontools
+sudo apt-get -y install dmidecode hwinfo python3 smartmontools wireless-tools mesa-utils x11-xserver-utils pcmciautils
 
 if ! which scan
 then
   (
+    here="`dirname \"$0\"`"
     echo
     echo "function scan() {"
-    echo "  `dirname \"$0\"`/scan.sh"
+    echo "  `realpath \"$here\"`/scan.sh \"\$@\""
     echo "}"
     echo
   ) >> ~/.bashrc
